dd8222d18d67d27336e51ba02961b6f934e94b1a
mahongtao <mahongtao_2000@hotmail.com>
2017-05-06 19:19:34 +0800

    增加watchPosition，clearWatch并且更新到最新版本

--- src/android/com/xunsoft/location/Location.java
+++ src/android/com/xunsoft/location/Location.java
@@ -6,9 +6,7 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import android.util.Log;
-
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;

8fff6fc2367ea34f8f8e800ac40da645e6224e57
mahongtao <mahongtao_2000@hotmail.com>
2017-05-26 22:01:48 +0800

    Update location.js

--- www/location.js
+++ www/location.js
@@ -1,18 +1,19 @@
+cordova.define("com-amap-location.Location", function(require, exports, module) {
 var exec = cordova.require('cordova/exec');
 module.exports = {
-    getCurrentPosition: function(success, error, args) { 
-    	console.log('getCurrentPosition');    
+    getCurrentPosition: function(success, error, args) {
+    	console.log('getCurrentPosition');
         exec(success, error, "Location", "getCurrentPosition", []);
     },
 
     watchPosition: function(success, error, args) {
-        console.log('watchPosition');    
+        console.log('watchPosition');
         exec(success, error, "Location", "watchPosition", []);
     },
 
-    clearWatch: function(pluginWatchId) {
-       console.log('clearWatch'); 
-       exec(success, error, "Location", "clearWatch", [pluginWatchId]);
+    clearWatch: function(success, error, args) {
+       console.log('clearWatch');
+       exec(success, error, "Location", "clearWatch", []);
     }
 };
-
+});
